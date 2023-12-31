da8531d7741c6f7b6cd663f877790d9e09f12e28
jrporto <joao6roberto@gmail.com>
2023-12-31 03:13:26 -0300

    Overall colorscheme changes and erasing, color-picking and rainbow mode functionalities implemented

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const grid = document.querySelector(".grid");
 
-// first we get the size of the grid only producing grids with
+let curMode = "color"
+let activeBtn = null
+let curColor = "black"
+
+// the grid only produces grids with
 // the same count of rows and collums, i.e. 2x2, 47x47, 88x88
 
-function getGridSize(){
-    let gridSize = prompt("Enter the number of collums/rows:");
-    generateGrid(gridSize);
-}
+// Random rgb value generator for rainbow mode implementation
 
+function getRndColor() {
+    let green = Math.floor((Math.random()*255));
+    let red = Math.floor((Math.random()*255));
+    let blue = Math.floor((Math.random()*255));
+    let rgb = `rgb(${green}, ${red}, ${blue})`
+    console.log(rgb)
+    return rgb
+}
 
 // then we generate the grid rows and its respective children
 
-function newRow(nodeCount){
-    let newRow = document.createElement("div");
+function newRow(nodeCount){ let newRow = document.createElement("div");
     newRow.classList.add("grid-row");   
     while(nodeCount--){
         let newNode = document.createElement("div");
-        newNode.style.border = "2px solid red";
         newNode.style.flex = "1 1 auto";
+        newNode.style.backgroundColor = "darkgrey";
         newNode.classList.add("hover-effect");
         newRow.appendChild(newNode);
         grid.appendChild(newRow);   
@@ -32,18 +40,33 @@ function generateGrid(gridSize){
     };
 }
 
-//getGridSize();
-
 // Now add event handlers to trigger css states
 
 function addPaintEffect(node){
-    node.addEventListener("mouseover",()=>{
-        node.style.backgroundColor = "black";
+    node.addEventListener("click",()=>{
+        switch (curMode) {
+            case "rainbow":
+                node.style.backgroundColor = getRndColor(); 
+                break;
+            case "eraser":
+                node.style.backgroundColor = "darkgrey";
+                break;
+            case "color":
+                node.style.backgroundColor = curColor;
+        }
     })
 }
 
-let gridSize = 4;
+let gridSize = 16;
 generateGrid(gridSize);
+
+// Function to clear grid
+
+function clearGrid() {
+    grid.querySelectorAll(".grid-row").forEach(row => {
+        grid.removeChild(row);
+    });
+}
 
 // Making the buttons change grid size
 
@@ -52,17 +75,71 @@ const minus = document.querySelector("#minus");
 
 plus.addEventListener("click", ()=>{
     gridSize++;
-    grid.querySelectorAll(".grid-row").forEach(row => {
-        grid.removeChild(row);
-    });
+    clearGrid()
     generateGrid(gridSize);
 })
 
 
 minus.addEventListener("click", ()=>{
     gridSize--;
-    grid.querySelectorAll(".grid-row").forEach(row => {
-        grid.removeChild(row);
-    });
+    clearGrid();
     generateGrid(gridSize);
 })
+
+// For the reset button
+
+const rstBtn = document.querySelector("#reset")
+
+rstBtn.addEventListener("click", () => {
+    clearGrid();
+    generateGrid(gridSize);
+});
+
+// Implementing rainbow mode
+
+const rnbBtn = document.querySelector("#rainbow")
+
+rnbBtn.addEventListener("click", () => {
+    curMode = "rainbow";
+    activateBtn(rnbBtn)
+})
+
+// Eraser option, resets the bg color to darkgrey
+
+const eraserBtn = document.querySelector("#eraser")
+
+eraserBtn.addEventListener("click", () => {
+    curMode = "eraser";
+    activateBtn(eraserBtn)
+})
+
+// Color picker for color mode
+
+const colorPicker = document.querySelector("#colorPicker")
+
+colorPicker.addEventListener("input", (e) => {
+    curColor = e.target.value;
+})
+
+// Color Mode button
+
+const colorBtn = document.querySelector("#color")
+
+colorBtn.onclick = () => {
+    activateBtn(colorBtn);
+    curMode = "color";
+}
+
+// Activating button color change
+
+function activateBtn(newMode) {
+    try {
+        activeBtn.classList.remove("active");
+        activeBtn = newMode;
+        activeBtn.classList.add("active");
+    }     
+    catch{
+        activeBtn = newMode;
+        activeBtn.classList.add("active");
+    }
+}
